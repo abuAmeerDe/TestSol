b6d4897d92f66522e4bebb845af4d4607b34af46
gbd.mohammad.afeef@outlook.com <gbd.mohammad.afeef@outlook.com>
2025-05-31 11:47:23 +0300

    com

--- Modules/HR.Attendances/Class1.cs
+++ Modules/HR.Attendances/Class1.cs
@@ -3,12 +3,5 @@
 public class Class1
 {
     // just for test
-<<<<<<< HEAD
     // add to first commit
-=======
-<<<<<<< HEAD
-    // add to first commit
-=======
->>>>>>> 2c1dd3bfecebdc2d24b123b123f32a66dc59acc2
->>>>>>> 5b9012658b983a3b82a04ae4fa8ed2f77bb83812
 }
